5f77b51be814297f68746b4fb09e626be16d8505
Simon pc2 <simonbarrau@gmail.com>
2022-11-11 01:53:04 -0300

    sweet

---
+++ netflix/netflix.js
@@ -0,0 +1,29 @@
+
+const clave= prompt("adivina la clave o sos cornudo")
+
+if(clave !== "-10"){
+    alert("perdoneme pero por ahora usted es cornudo, pista: es un numero")
+}else{
+    alert(" si la adivinaste aca es xq sos un puto nazi!")
+}
+const clave2=prompt("dale adivinaaa");
+ if (clave2 !=="-10"){
+    alert("Seguis siendo cornudo boludoo, ADIVINAAA LA CLAVEE. Otra pista:  es numero negativo")
+}else{
+    alert(" Bieeeen! No sos cornudo, te salvaste! podes ir al donpla con los pibes 😁😎")
+}
+
+const clave3= prompt("sos boludoo???, dale adivina")
+
+if(clave3 !== "-10") {
+alert("bueno, parece q toca volver al gym. Te doy otra pista: Es un numero usado por los mejores jugadores de la seleccion, pero negativo")
+}else{
+    alert(" Vamaaaa, te saluda brianeitor! salame")
+}
+
+const clave4=prompt("si no la sacas ahora es xq queres ser cornudo")
+if(clave4 !== "-10"){
+    alert("Listo perdiste x la cara de dolobu")
+} else{
+    alert(" Bieeeen! te llevo 4 intentos pero lo bueno es q te salvaste!")
+}
